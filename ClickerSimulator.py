2a30bbd0a47bde0015dedae4ce1b69534a71e813
c2rcuit <matthew.zaveckas@gmail.com>
2025-07-16 14:57:31 +0300

    Update ClickerSimulator.py

--- ClickerSimulator.py
+++ ClickerSimulator.py
@@ -1,8 +1,11 @@
 from tkinter import *
+import time
 
 totalclicks = 0
-soggycat = None
-versalink = "https://github.com/c2rcuit/Versa"
+cat = None
+
+autoclicker_enabled = False
+
 
 def click():
     global totalclicks
@@ -21,6 +24,14 @@ def clickx4():
     totalclicks += 8
     label.config(text=f"{totalclicks}")
     finishgame()
+
+def autoclicker():
+    global totalclicks, autoclicker_enabled
+    if autoclicker_enabled:
+        totalclicks += 1
+        label.config(text=f"{totalclicks}")
+        finishgame()
+        window.after(100, autoclicker)
 
 def getx2clicks():
     global totalclicks
@@ -41,19 +52,28 @@ def getx4clicks():
         finishgame()
     else:
         print("not enough money")
+def getautoclicker():
+    global totalclicks, autoclicker_enabled
+    if totalclicks >= 1000:
+        totalclicks -= 1000
+        autoclicker_enabled = True
+        label.config(text=f"{totalclicks}")
+        buttonauto.config(state=DISABLED, text=" ", font=("Arial", 0))
+        autoclicker()
 
 def finishgame():
-    global totalclicks, soggycat
-    if totalclicks >= 500:
-        if soggycat is None:
-            soggycat = PhotoImage(file="funnypicture.png")
+    global totalclicks, cat, autoclicker_enabled
+    if totalclicks >= 5000:
+        if cat is None:
+            cat = PhotoImage(file="funnypicture.png")
             label.config(text="Thank You For Playing!")
-            button.config(text=f"Go get versa here! {versalink}")
             button.config(fg="black")
             button.config(font=("Arial",30))
             button.config(state=DISABLED)
-            buttonx2.config(image=soggycat)
+            buttonx2.config(image=cat)
             buttonx2.config(state=DISABLED, text="", font=("Arial", 50))
+            buttonauto.config(state=DISABLED, text=" ", font=("Arial",0))
+            autoclicker_enabled = False
 
 window = Tk()
 window.geometry("1200x1000")
@@ -68,6 +88,11 @@ buttonx2 = Button(window,text="Click Me To Get X2 Clicks!(100)")
 buttonx2.config(font=("Arial",50,))
 buttonx2.config(command=getx2clicks)
 buttonx2.place(relx=0.5, y=150, anchor='n')
+
+buttonauto = Button(window,text="Click Me To Get A AutoClicker(1000)")
+buttonauto.config(font=("Arial",50,))
+buttonauto.config(command=getautoclicker)
+buttonauto.place(relx=0.5, y=230, anchor='n')
 
 label = Label(window,
               text=f"{totalclicks}",
